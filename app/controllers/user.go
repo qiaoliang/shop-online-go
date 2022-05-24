3502cc3c86a1d1fb012c70df0dab1b8a4315b4cc
qiaoliang <sagittatius.qiao@gmail.com>
2022-05-24 22:52:16 +0800

    login with a fix token, which is 'iamToken'.

--- app/controllers/user.go
+++ app/controllers/user.go
@@ -7,9 +7,9 @@ import (
 
 func UserLogin(c *gin.Context) {
 	result := initUserData()
-	c.JSON(http.StatusOK, gin.H{"data": &result})
+	c.JSON(http.StatusOK, gin.H{"code":0,"data": &result,"msg":"OK"})
 }
 
-func initUserData() string {
-	return "ok";
+func initUserData() interface{} {
+	return map[string]string{"token":"iamToken",}
 }
