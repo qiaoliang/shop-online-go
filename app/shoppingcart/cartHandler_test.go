f3e36c5e23e84a5fcd680cdadf1c25e0643e9241
qiaoliang <sagittatius.qiao@gmail.com>
2022-06-01 20:41:39 +0800

    fix: User String comparation as Test Assertion, rather than structs.

--- app/shoppingcart/cartHandler_test.go
+++ app/shoppingcart/cartHandler_test.go
@@ -52,31 +52,20 @@ func (st *ShoppingCartHandlerSuite) Test_update_volume_of_item_in_shoppingcart_f
 	data.Add("gid", "gid")
 
 	exp := `{"code":0,"data":{"token":"iamTestToken7896554","cartInfo":"iamInfos","number":5,"items":[{"key":1,"gid":"gid","pic":"http://localhost:9090/pic/goods/g7225946-01.jpeg","status":0,"name":"CD1.0","sku":["sku1","sku3"],"price":66,"number":5,"selected":"1","optionValueName":"valueName"}],"goods":[{"goodsId":1,"number":5}]},"msg":"OK"}`
-	var expResult CartJson
-	utils.JsonToStruct(exp, expResult)
-
-	var realResult CartJson
 	body := string(utils.HttpPost(st.router, data, "/v1/shopping-cart/modifyNumber"))
-	utils.JsonToStruct(body, realResult)
-	st.EqualValues(expResult.Data, expResult.Data)
+	st.Equal(exp, string(body))
 }
 
 func (st *ShoppingCartHandlerSuite) Test_get_cart_for_unexisted_token() {
 
 	exp := `{"code":0,"data":"","msg":"OK"}`
-	var expResult CartJson
-	utils.JsonToStruct(exp, expResult)
-
 	//构建参数
 	data := map[string]string{
 		"token": "UnexistedToken",
 	}
-
 	body := utils.HttpGet("/v1/shopping-cart/info", data, st.router)
 
-	var realResult CartJson
-	utils.JsonToStruct(string(body), realResult)
-	st.EqualValuesf(expResult, realResult, " expect:%v\nactual:%v\n", expResult, realResult)
+	st.Equal(exp, string(body))
 }
 
 func setupTestRouter() *gin.Engine {
